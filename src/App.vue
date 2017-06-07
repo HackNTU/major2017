b166027a8133aebf5c9f468ee369e9b2c93740f2
ooookai <ooookai@gmail.com>
2017-06-08 01:24:21 +0800

    set index.html <head> and GA Script
    using vue-head

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,45 @@ export default {
     return {
       msg: 'Welcome!'
     }
-  }
+  },
+  head: {
+    meta: [
+      // Facebook / Open Graph
+      {
+        property: 'og:title',
+        content: 'Content Title'
+      }, {
+        property: 'og:url',
+        content: 'http://major.hackntu.taipei'
+      }, {
+        property: 'og:type',
+        content: 'website'
+      }, {
+        property: 'og:title',
+        content: '臺大黑客松 HackNTU'
+      }, {
+        property: 'og:description',
+        content: '7/21-23 (花博公園爭艷館)，三天Hacking不間斷！'
+      }, {
+        property: 'og:image',
+        content: './src/assets/logo/banner.jpg'
+      },
+    ],
+    link: [
+      // Favicon
+      {
+        rel: 'shortcut icon',
+        href: require('@/assets/logo/favicon.png'),
+        sizes: '16x16',
+        type: 'image/png'
+      }, {
+        rel: 'icon',
+        href: require('@/assets/logo/favicon.png'),
+        sizes: '16x16',
+        type: 'image/png'
+      },
+    ],
+  },
 }
 </script>
 <style lang="scss">
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import VueHead from 'vue-head'
+
 import App from './App.vue'
+
+Vue.use(VueHead)
 
 new Vue({
   el: '#app',
