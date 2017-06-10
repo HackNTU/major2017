b54d81af041e982bb39061c5a8139ef6ab949557
ooookai <ooookai@gmail.com>
2017-06-10 17:34:35 +0800

    try handle move, resize, and orientation

--- src/main.js
+++ src/main.js
@@ -9,3 +9,23 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+
+
+/*----------  js control  ----------*/
+
+// lock screen orientation
+screen.lockOrientation('portrait');
+
+
+/*----------  events  ----------*/
+
+// disable touchmove
+document.addEventListener("touchmove", function(event) {
+  event.preventDefault();
+}, false);
+
+window.addEventListener('resize', function() {
+  window.scrollTo(0, 1);
+}, true);
